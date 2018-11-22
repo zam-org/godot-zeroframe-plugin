b081acef5db97f880c076f3e5d7d782b108aaec1
lentsius <krystofklestil@live.com>
2018-11-23 00:39:02 +0100

    ZeroFrame is now a panel on the right.

--- addons/ZeroFrame/ZeroFrame_addon.gd
+++ addons/ZeroFrame/ZeroFrame_addon.gd
@@ -1,8 +1,17 @@
 tool
 extends EditorPlugin
 
+var dock
+
 func _enter_tree():
 	add_custom_type("ZeroFrame", "Node", load("res://addons/ZeroFrame/ZeroFrame.gd"), load("res://addons/ZeroFrame/icon.png"))
+	
+	dock = preload("res://addons/ZeroFrame/ZeroFrame_ui.tscn").instance()
+
+	# Add the loaded scene to the docks
+	add_control_to_dock(DOCK_SLOT_RIGHT_UL, dock)
 
 func _exit_tree():
-	pass
+	remove_control_from_docks(dock)
+	# Erase the control from the memory
+	dock.queue_free()
